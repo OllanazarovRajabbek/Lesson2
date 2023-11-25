e16c8235e81d0e6a66d3810a07298da81c4683a5
Rajabbek Ollanazarov <ollanzarovrajabbek2@gmail.com>
2023-11-25 18:19:55 +0500

    3 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+    public static void m19(){
+        System.out.println("1-method");
+    }
+    public static void m2(){
+        System.out.println("2-method");
     }
 }
